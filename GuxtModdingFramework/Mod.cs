5264cf195360bd1c3f0eecb8e67cb0d870840d0f
Brayconn <29030756-Brayconn@users.noreply.github.com>
2019-09-24 13:44:39 -0700

    Mod should be fully fleshed out now (except for modifying the exe)

--- GuxtModdingFramework/Mod.cs
+++ GuxtModdingFramework/Mod.cs
@@ -1,49 +1,173 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace GuxtModdingFramework
 {
     public class Mod
     {
+        #region File Extension stuff
+
+        public const string DefaultMapExtension = "pxmap";
+        public const string DefaultEntityExtension = "pxeve";
+        public const string DefaultImageExtension = "pximg";
+        public const string DefaultAttributeExtension = "pxatrb";
+        public const string DefaultProjectExtension = "stgprj";
+
+        private string mapExtension = DefaultMapExtension;
+        private string entityExtension = DefaultEntityExtension;
+        private string imageExtension = DefaultImageExtension;
+        private string attributeExtension = DefaultAttributeExtension;
+        private string projectExtension = DefaultProjectExtension;
+
+        [Category("File Extensions"), DefaultValue(DefaultMapExtension)]
+        public string MapExtension
+        {
+            get => mapExtension;
+            set
+            {
+                mapExtension = value;
+                UpdateLists();
+            }
+        }
+
+        [Category("File Extensions"), DefaultValue(DefaultEntityExtension)]
+        public string EntityExtension
+        {
+            get => entityExtension;
+            set
+            {
+                entityExtension = value;
+                UpdateLists();
+            }
+        }
+        
+        [Category("File Extensions"), DefaultValue(DefaultImageExtension)]
+        public string ImageExtension
+        {
+            get => imageExtension;
+            set
+            {
+                imageExtension = value;
+                UpdateLists();
+            }
+        }
+        
+        [Category("File Extensions"), DefaultValue(DefaultAttributeExtension)]
+        public string AttributeExtension
+        {
+            get => attributeExtension;
+            set
+            {
+                attributeExtension = value;
+                UpdateLists();
+            }
+        }
+        
+        [Category("File Extensions"), DefaultValue(DefaultProjectExtension)]
+        public string ProjectExtension
+        {
+            get => projectExtension;
+            set
+            {
+                projectExtension = value;
+                UpdateLists();
+            }
+        }
+
+        #endregion
+
+        [Category("General"), Description("Whether or not the images are scrambeled or not. Only turn this off if you've patched your exe and converted the images.")]
         public bool ImagesScrambeled { get; set; } = true;
 
+        [Category("General"), ReadOnly(true), Description("Path to the data folder of your mod. This should only be changed in the event something gets desynced.")]
         public string DataPath { get; set; }
 
-        public List<string> Maps { get; set; }
-        public List<string> Entities { get; set; }
-        public List<string> Images { get; set; }
-        public List<string> Attributes { get; set; }
-        public List<string> Projects { get; set; }
+        #region Internal lists
+
+        [Browsable(false)]
+        public BindingList<string> Maps { get; } = new BindingList<string>();
+        [Browsable(false)]
+        public BindingList<string> Entities { get; } = new BindingList<string>();
+        [Browsable(false)]
+        public BindingList<string> Images { get; } = new BindingList<string>();
+        [Browsable(false)]
+        public BindingList<string> Attributes { get; } = new BindingList<string>();
+        [Browsable(false)]
+        public BindingList<string> Projects { get; } = new BindingList<string>();
+
+        #endregion
+
+        private void UpdateLists()
+        {
+            static void FillWithFileNames(BindingList<string> list, string dir, string filter)
+            {
+                list.Clear();
+                foreach (var f in Directory.EnumerateFiles(dir, "*." + filter))
+                    list.Add(Path.GetFileName(f));
+            }
+
+            FillWithFileNames(Maps, DataPath, mapExtension);
+            FillWithFileNames(Entities, DataPath, EntityExtension);
+            FillWithFileNames(Images, DataPath, ImageExtension);
+            FillWithFileNames(Attributes, DataPath, AttributeExtension);
+            FillWithFileNames(Projects, DataPath, ProjectExtension);
+        }
 
         public static Mod FromDataFolder(string path)
         {
-            return new Mod()
-            {
-                DataPath = path,
-
-                Maps = Directory.GetFiles(path, "*.pxmap").Select(x => Path.GetFileName(x)).ToList(),
-
-                Entities = Directory.GetFiles(path, "*.pxeve").Select(x => Path.GetFileName(x)).ToList(),
-
-                Images = Directory.GetFiles(path, "*.pximg").Select(x => Path.GetFileName(x)).ToList(),
-
-                Attributes = Directory.GetFiles(path, "*.pxatrb").Select(x => Path.GetFileName(x)).ToList(),
-
-                Projects = Directory.GetFiles(path, "*.stgprj").Select(x => Path.GetFileName(x)).ToList()
-            };
+            if(!Directory.Exists(path))
+                //TODO don't make me xml edit
+                throw new DirectoryNotFoundException($"The directory \"{path}\" was not found. Please fix it using an xml editor.");
+            
+            var m = new Mod() { DataPath = path };
+            m.UpdateLists();
+            return m;
         }
 
         public void Save(string path)
         {
-            File.WriteAllText(path, (ImagesScrambeled ? "1" : "0") + DataPath);
+            new XDocument(
+                new XElement("GuxtMod",
+                    new XElement("ImagesScrambeled", ImagesScrambeled),
+                    new XElement("DataPath", DataPath),
+                    new XElement("FileExtensions",
+                        new XElement("Map", MapExtension),
+                        new XElement("Entity", EntityExtension),
+                        new XElement("Images", ImageExtension),
+                        new XElement("Attributes", AttributeExtension),
+                        new XElement("Projects", ProjectExtension)
+                   )
+                )
+            ).Save(path);
         }
 
         public static Mod Load(string path)
         {
-            string projFile = File.ReadAllText(path);
-            Mod m = FromDataFolder(projFile.Substring(1));
-            m.ImagesScrambeled = projFile[0] == '1';
+            var doc = new XmlDocument();
+            doc.Load(path);
+
+            string? dataFolder = doc.SelectSingleNode("GuxtMod/DataPath")?.InnerText;
+            if(string.IsNullOrWhiteSpace(dataFolder))
+                throw new ArgumentNullException("DataPath","The selected Guxt Mod doesn't have a data folder.");
+            if (!Directory.Exists(dataFolder))
+                //TODO don't make me xml edit
+                throw new DirectoryNotFoundException($"The directory \"{dataFolder}\" was not found. Please fix it using an xml editor.");
+
+            Mod m = FromDataFolder(dataFolder);
+            m.ImagesScrambeled = doc.SelectSingleNode("GuxtMod/ImagesScrambeled")?.InnerText == "true";
+
+            var extensions = doc.SelectSingleNode("GuxtMod/FileExtensions");
+            m.MapExtension = extensions.SelectSingleNode("Map")?.InnerText ?? m.MapExtension;
+            m.EntityExtension = extensions.SelectSingleNode("Entity")?.InnerText ?? m.EntityExtension;
+            m.ImageExtension = extensions.SelectSingleNode("Image")?.InnerText ?? m.ImageExtension;
+            m.AttributeExtension = extensions.SelectSingleNode("Attribute")?.InnerText ?? m.AttributeExtension;
+            m.ProjectExtension = extensions.SelectSingleNode("Projects")?.InnerText ?? m.ProjectExtension;
+            
             return m;
         }
     }
